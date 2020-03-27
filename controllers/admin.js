f066dfd7a8a11ddd96867cf672b9aa2ceadde106
muhrianiqbal <muhrian.iqbal@gmail.com>
2020-03-27 08:11:04 +0700

    CRUD Done

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,13 +15,10 @@ class AdminController
         let data = {name, username, password, address, email, phone};
         bcrypt.genSalt(saltRounds, function(err, salt) {
             bcrypt.hash(data.password, salt, function(err, hash) {
-                // Store hash in your password DB.
                 data.password = hash;
-                // console.log(data)
                 console.log(data)
                 Admin.create(data)
                 .then(() => res.redirect("/admin"))
-                // .then(data => res.send(data))
                 .catch(err => res.send(err));
             });
         });
@@ -56,20 +53,15 @@ class AdminController
     {
         Order.findAll({include : [Patient, Medicine]})
         .then(data => res.render("./admin/order", {data}))
-        // .then(data =>  res.send(data))
         .catch(err => res.send(err))
     }  
 
     static update(req, res)
     {
         let {information} = req.body;
-        let {PatientId, MedicineId} = req.query;
         let {id} = req.params;
-        console.log(req.params)
         Order.update({information}, {where : {id}})
-        // Order.findAll()
-        .then(data =>  res.send(data))
-        // .then(() => res.redirect("/admin/order"))
+        .then(() => res.redirect("/admin/order"))
         .catch(err => res.send(err))
     }
 
